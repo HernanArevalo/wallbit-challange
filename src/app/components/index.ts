8602433330a66d9853a3f9cb28634e44a9cc82b0
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-11-25 18:10:45 -0300

    create new-product component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,1 +1,2 @@
 export * from './divider/Divider'
+export * from './new-product/NewProduct'
---
+++ src/app/components/new-product/NewProduct.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { MdOutlineLibraryAdd } from 'react-icons/md'
+import './styles.css'
+
+
+export const NewProduct = () => {
+
+  return (
+    <div className='new-product'>
+      <div>
+        <div>
+          <MdOutlineLibraryAdd size={30} />
+        </div>
+      </div>
+    </div>
+  )
+}
